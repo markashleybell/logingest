ed13c39cdcea65701decf7b994f81fc4878ef5cd
Mark Bell <me@markb.co.uk>
2021-08-07 18:13:07 +0100

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::fs::{self, File};
-use std::path::{PathBuf};
+use std::path::{Path, PathBuf};
 use std::io::{BufReader, BufRead, BufWriter, Read};
 
 use anyhow::{Context, Result};
+use csv::Writer;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -13,6 +14,50 @@ struct Arguments {
     /// The output CSV file
     #[structopt(short, long, parse(from_os_str))]
     output: PathBuf,
+}
+
+fn rewrite_csv(path: &Path, writer: &mut Writer<BufWriter<File>>) -> Result<(), Box<dyn std::error::Error>> {
+    let source_log_file = File::open(path)
+        .with_context(|| format!("Could not read file `{}`", path.display()))?;
+    
+    let mut reader = BufReader::with_capacity(128 * 1024, source_log_file);
+
+    // Skip the IIS log header (4 lines)
+    reader.by_ref().lines().take(4).for_each(drop);
+
+    let mut csv = csv::ReaderBuilder::new()
+        .delimiter(b' ')
+        .from_reader(reader);
+
+    for result in csv.records() {
+        let record = result
+            .with_context(|| format!("Couldn't read source row in `{}`", path.display()))?;
+        
+        let c = format!("{} {}", &record[0], &record[1]);
+
+        // SURELY there must be a cleaner way to do this??
+        let n = [
+            &c,
+            &record[2],
+            &record[3],
+            &record[4],
+            &record[5],
+            &record[6],
+            &record[7],
+            &record[8],
+            &record[9],
+            &record[10],
+            &record[11],
+            &record[12],
+            &record[13],
+            &record[14]
+        ];
+
+        writer.write_record(n)
+            .with_context(|| format!("Couldn't write row to `{}`", path.display()))?;
+    }
+
+    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -55,6 +100,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     csv_writer.write_record(headers)
         .with_context(|| format!("Couldn't write header row to `{}`", output_csv_path.display()))?;
 
+    let csv_writer_ref = &mut csv_writer;
+
     if source_log_path.is_dir() {
         let entries = fs::read_dir(source_log_path)
             .with_context(|| format!("Couldn't read files from `{}`", source_log_path.display()))?;
@@ -62,90 +109,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         for e in entries {
             let entry = e?;
             let path = entry.path();
-            let pb = path.as_path();
 
-            if entry.path().is_file() {
-                let source_log_file = File::open(pb)
-                    .with_context(|| format!("Could not read file `{}`", pb.display()))?;
-                    
-                let mut reader = BufReader::with_capacity(128 * 1024, source_log_file);
-
-                // Skip the IIS log header (4 lines)
-                reader.by_ref().lines().take(4).for_each(drop);
-
-                let mut csv = csv::ReaderBuilder::new()
-                    .delimiter(b' ')
-                    .from_reader(reader);
-
-                for result in csv.records() {
-                    let record = result
-                        .with_context(|| format!("Couldn't read source row in `{}`", path.display()))?;
-                    
-                    let c = format!("{} {}", &record[0], &record[1]);
-
-                    // SURELY there must be a cleaner way to do this??
-                    let n = [
-                        &c,
-                        &record[2],
-                        &record[3],
-                        &record[4],
-                        &record[5],
-                        &record[6],
-                        &record[7],
-                        &record[8],
-                        &record[9],
-                        &record[10],
-                        &record[11],
-                        &record[12],
-                        &record[13],
-                        &record[14]
-                    ];
-
-                    csv_writer.write_record(n)
-                        .with_context(|| format!("Couldn't write row to `{}`", output_csv_path.display()))?;
-                }
+            if path.is_file() {
+                rewrite_csv(path.as_path(), csv_writer_ref)?;
             }
         }
     } else {
-        let source_log_file = File::open(source_log_path)
-            .with_context(|| format!("Could not read file `{}`", source_log_path.display()))?;
-            
-        let mut reader = BufReader::with_capacity(128 * 1024, source_log_file);
-
-        // Skip the IIS log header (4 lines)
-        reader.by_ref().lines().take(4).for_each(drop);
-
-        let mut csv = csv::ReaderBuilder::new()
-            .delimiter(b' ')
-            .from_reader(reader);
-
-        for result in csv.records() {
-            let record = result
-                .with_context(|| format!("Couldn't read source row in `{}`", source_log_path.display()))?;
-            
-            let c = format!("{} {}", &record[0], &record[1]);
-
-            // SURELY there must be a cleaner way to do this??
-            let n = [
-                &c,
-                &record[2],
-                &record[3],
-                &record[4],
-                &record[5],
-                &record[6],
-                &record[7],
-                &record[8],
-                &record[9],
-                &record[10],
-                &record[11],
-                &record[12],
-                &record[13],
-                &record[14]
-            ];
-
-            csv_writer.write_record(n)
-                .with_context(|| format!("Couldn't write row to `{}`", output_csv_path.display()))?;
-        }
+        rewrite_csv(source_log_path, csv_writer_ref)?;
     }
 
     Ok(())
